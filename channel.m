cded3faa4259f0f16be2da7eff5cc700b9308774
Jyothi53 <141492413+Jyothi53@users.noreply.github.com>
2024-05-09 15:21:58 +0530

    Add files via upload

--- channel.m
+++ channel.m
@@ -1,6 +1,15 @@
-function r_t = channel (s_t)
-    r_t = zeros(2, length(s_t));
-    for idx = 1:length(s_t)
-        r_t(1,idx) = s_t(idx) + 
+function r_t = channel (s_t, type, t1, Ts, Fs)
+    standard_deviation = 1;
+    if (type == "Memoryless")
+        r_t = s_t + sqrt(standard_deviation) * randn(size(s_t));
+    elseif (type == "Memory")
+        a = 0.3;
+        b = 1;
+        h_t = a * (t1 == 0) + (1 - a) * (t1 == b*Fs*Ts);
+        rx_t = conv(h_t, s_t);
+        r_t = rx_t + sqrt(standard_deviation) * randn(size(rx_t));
+        r_t = r_t(1:length(t1));
+    else
+        disp('Invalid argument type');
     end
 end
